f22287e48ababb1470a604c1d9f80c279e2567c7
SylvesterNdwata <ndwatasylvester88@gmail.com>
2026-02-05 19:54:00 +0100

    Sample menu system

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        @SuppressWarnings("resource")
         Scanner reader = new Scanner(System.in);
+        PasswordManager manager = new PasswordManager();
         System.out.println("Welcome to the Password Manager");
 
         while (true) {
@@ -17,37 +17,48 @@ public class Main {
                 break;
             }
 
-            if (response.equals("1")) {
-
-                System.out.print("What service's password would you like to save? ");
-                String service = reader.nextLine();
-
-                if (service.isEmpty()) {
+            switch (response) {
+                case "1":
+                    addPassword(reader, manager);
                     break;
-                }
 
-                System.out.print("What is the username? ");
-                String username = reader.nextLine();
+                case "2":
+                    searchPassword(reader, manager);
+                    break;
 
-                System.out.print("What is the password? ");
-                String password = reader.nextLine();
-
-                PasswordEntry entry = new PasswordEntry(service, username, password);
-
-                System.out.println(entry);
-
-                PasswordManager manager = new PasswordManager();
-                manager.saveToFile(entry);
-            } else if (response.equals("2")) {
-                PasswordManager extract = new PasswordManager();
-
-                System.out.print("What service password are you looking for? ");
-                String serviceNeeded = reader.nextLine();
-
-                extract.loadFromFile(serviceNeeded);
+                default:
+                    System.out.println("Invalid option, Please try again.");
             }
 
         }
 
     }
+
+    private static void addPassword(Scanner reader, PasswordManager manager) {
+        System.out.print("What service's password would you like to save? ");
+        String service = reader.nextLine();
+
+        if (service.isEmpty()) return;
+
+        System.out.print("What is the username? ");
+        String username = reader.nextLine();
+
+        System.out.print("What is the password? ");
+        String password = reader.nextLine();
+
+        PasswordEntry entry = new PasswordEntry(service, username, password);
+
+        System.out.println(entry);
+        manager.saveToFile(entry);
+    }
+
+    private static void searchPassword(Scanner reader, PasswordManager manager) {
+        System.out.print("What service password are you looking for? ");
+        String serviceNeeded = reader.nextLine();
+        if (serviceNeeded.isEmpty()) return; 
+
+        manager.loadFromFile(serviceNeeded);
+    }
+
+    
 }
